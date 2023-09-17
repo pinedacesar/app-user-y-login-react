1457a3cc55647fa770a6dcacd914c90038f6e880
pinedacesar <cesar.pineda.assert@gmail.com>
2023-09-16 22:17:18 -0300

    se agrega el login y auth

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,52 +1,37 @@
-import { UserModalForm } from './components/UserModalForm';
-import { UsersList } from './components/UsersList';
-import { useUsers } from './hooks/useUsers';
+import { useReducer } from 'react';
+import { LoginPage } from './auth/pages/LoginPage';
+import { UsersPages } from './pages/UserPages';
+import { loginReducer } from './auth/reducers/loginReducer';
+import Swal from 'sweetalert2';
+
+const initialLogin = {
+  isAuth: false,
+  user: undefined,
+};
 
 export const UsersApp = () => {
-  const {
-    users,
-    userSelected,
-    initialUsersForm,
-    visibleForm,
-    handlerAddUser,
-    handlerRemoveUser,
-    handlerUserSelectedForm,
-    handlerOpenForm,
-    handlerCloseForm,
-  } = useUsers();
+  const [login, dispach] = useReducer(loginReducer, initialLogin);
+
+  const handlerLogin = ({ username, password }) => {
+    if (username === 'admin' && password === '12345') {
+      const user = { username: 'admin' };
+      dispach({
+        type: 'login',
+        payload: user,
+      });
+      console.log('logueado');
+    } else {
+      Swal.fire('Error Login', 'Username y password invalidos', 'error');
+    }
+  };
 
   return (
     <>
-      {!visibleForm || (
-        <UserModalForm
-          handlerAddUser={handlerAddUser}
-          initialUsersForm={initialUsersForm}
-          userSelected={userSelected}
-          handlerCloseForm={handlerCloseForm}
-        />
+      {login.isAuth ? (
+        <UsersPages />
+      ) : (
+        <LoginPage handlerLogin={handlerLogin} />
       )}
-      <div className="container my-4">
-        <h1>Users App</h1>
-        <div className="row">
-          <div className="col">
-            <button className="btn btn-primary my-2" onClick={handlerOpenForm}>
-              Nuevo Usuario
-            </button>
-
-            {users.length === 0 ? (
-              <div className="alert alert-warning">
-                No hay usuarios en el sistema
-              </div>
-            ) : (
-              <UsersList
-                users={users}
-                handlerRemoveUser={handlerRemoveUser}
-                handlerUserSelectedForm={handlerUserSelectedForm}
-              />
-            )}
-          </div>
-        </div>
-      </div>{' '}
     </>
   );
 };
---
+++ src/pages/UserPages.jsx
@@ -0,0 +1,52 @@
+import { UserModalForm } from '../components/UserModalForm';
+import { UsersList } from '../components/UsersList';
+import { useUsers } from '../hooks/useUsers';
+
+export const UsersPages = () => {
+  const {
+    users,
+    userSelected,
+    initialUsersForm,
+    visibleForm,
+    handlerAddUser,
+    handlerRemoveUser,
+    handlerUserSelectedForm,
+    handlerOpenForm,
+    handlerCloseForm,
+  } = useUsers();
+
+  return (
+    <>
+      {!visibleForm || (
+        <UserModalForm
+          handlerAddUser={handlerAddUser}
+          initialUsersForm={initialUsersForm}
+          userSelected={userSelected}
+          handlerCloseForm={handlerCloseForm}
+        />
+      )}
+      <div className="container my-4">
+        <h1>Users App</h1>
+        <div className="row">
+          <div className="col">
+            <button className="btn btn-primary my-2" onClick={handlerOpenForm}>
+              Nuevo Usuario
+            </button>
+
+            {users.length === 0 ? (
+              <div className="alert alert-warning">
+                No hay usuarios en el sistema
+              </div>
+            ) : (
+              <UsersList
+                users={users}
+                handlerRemoveUser={handlerRemoveUser}
+                handlerUserSelectedForm={handlerUserSelectedForm}
+              />
+            )}
+          </div>
+        </div>
+      </div>{' '}
+    </>
+  );
+};
